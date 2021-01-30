2c075568f393e64f0edca3a100237de218a7910a
Boris Serebrov <serebrov@gmail.com>
2021-01-30 13:39:10 +0200

    Add picker example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
+
+  <div class="row">
+    <StaticPicker :data="emojiIndex" set="twitter" @select="showEmoji" />
+  </div>
+
+  <div class="row">{{ emojisOutput }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* import HelloWorld from './components/HelloWorld.vue' */
+
+/* import data from "emoji-mart-vue-fast/data/all.json"; */
+/* import { Picker, EmojiIndex } from "emoji-mart-vue-fast"; */
+/* import "emoji-mart-vue-fast/css/emoji-mart.css"; */
+import data from "../../emoji-mart-vue/data/all.json";
+import { StaticPicker, EmojiIndex } from "../../emoji-mart-vue/src";
+import "../../emoji-mart-vue/css/emoji-mart.css";
+
+let emojiIndex = new EmojiIndex(data);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    /* HelloWorld, */
+    StaticPicker
+  },
+
+  data() {
+    return {
+      emojiIndex: emojiIndex,
+      emojisOutput: ""
+    };
+  },
+
+  methods: {
+    showEmoji(emoji) {
+      this.emojisOutput = this.emojisOutput + emoji.native;
+    }
   }
-}
+};
 </script>
 
 <style>
